0fddf337ca487f17ab7db938dda31df8ec6cbe2d
tifero <tifero@mail.ru>
2019-06-10 02:07:18 +0300

    Первая попытка

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public int destoyDelay;
+    private TextMeshProUGUI _loose;
+
+    private void Start()
+    {
+        _loose = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        ScoreCount.Score++;
+    }
+
+    private void Update()
+    {
+        StartCoroutine(DestroyObj());
+    }
+
+    private IEnumerator DestroyObj()
+    {
+        yield return new WaitForSeconds(destoyDelay);
+        Destroy(gameObject);
+        ScoreCount.Score--;
+    }
+}
