6091597613e55dbe04978879a080aff6c1a9fef2
tifero <tifero@mail.ru>
2019-06-23 20:49:33 +0300

    Заливка ассетов

---
+++ Assets/Scripts/DestroyByTme.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTme : MonoBehaviour
+{
+    private const float DestroyDelay = 1f;
+
+    private void Update()
+    {
+        StartCoroutine(DestroyCircle());
+    }
+    
+    private IEnumerator DestroyCircle()
+    {
+        yield return new WaitForSeconds(DestroyDelay);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausemenuUi;
+
+    private void Start()
+    {
+        pausemenuUi.SetActive(false);
+    }
+
+    private void Update()
+    {
+        GetButtonPause();
+    }
+
+    private void GetButtonPause()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape)) return;
+        if (Manager.CanPlay)
+        {
+            Pause();
+        }
+        else
+        {
+            Resume();
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+        Manager.CanPlay = false;
+        pausemenuUi.SetActive(true);
+    }
+
+    private void Resume()
+    {
+        Time.timeScale = 1f;
+        Manager.CanPlay = true;
+        pausemenuUi.SetActive(false);
+    }
+
+    public void ResumePlayButton()
+    {
+        Resume();
+    }
+}
